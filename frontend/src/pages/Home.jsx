191aa6631316b29464e94402fb2b1265e767e817
ozalwaraditya <adityaozalwar03@gmail.com>
2025-01-20 23:41:24 +0530

    Added Speciality Menu

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header.jsx";
+import SpecialityMenu from "../components/SpecialityMenu.jsx";
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <SpecialityMenu/>
     </div>
   )
 }
