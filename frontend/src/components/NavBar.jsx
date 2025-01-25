7f6568a295704ce52a82a59253b83555fd7fb614
ozalwaraditya <adityaozalwar03@gmail.com>
2025-01-25 21:04:43 +0530

    Added All Doctor page
    Filter checking is also applied.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-        <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+        <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt="" />
         <ul className='hidden md:flex items-start gap-5 font-medium '>
             <NavLink to='/'>
                 <h1 className = "py-1">HOME</h1>
